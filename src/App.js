82f3511d02a3f3c09151aa7257e87ac3d3726cdb
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-09-15 19:12:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <button onClick={increment}>increment</button>
+      <h2>count is now {count} !</h2>
+    </div>
+  );
+}
